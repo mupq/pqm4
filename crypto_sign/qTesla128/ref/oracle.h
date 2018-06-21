9aec8b1a1f983c408aa7f64d2474a0ca14ddbcba
erdemalkim <erdem.alkim@bil.omu.edu.tr>
2018-06-21 10:49:45 +0300

    qTesla128 implementation removed

--- crypto_sign/qTesla128/ref/oracle.h
+++
@@ -1,9 +0,0 @@
-#ifndef ORACLE_H
-#define ORACLE_H
-
-#include "params.h"
-#include "poly.h"
-
-void random_oracle(unsigned char *c_bin, poly v, const unsigned char *m, unsigned long long mlen);
-
-#endif
--- crypto_sign/qTesla128/ref/sign.h
+++
@@ -1,4 +0,0 @@
-int get_sig_rejections_wi(void);
-void delete_sig_rejections_wi(void);
-int get_sig_rejections_final(void);
-void delete_sig_rejections_final(void);
