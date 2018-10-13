abc55c05bf55d17d8dac7507a95b687f91d6cbce
Matthias J. Kannwischer <matthias@kannwischer.eu>
2018-10-13 12:13:15 +0200

    fixes markdown generation if benchmarks are one line off
    could be done more elegantly

--- benchmarks_to_md.py
+++ benchmarks_to_md.py
@@ -6,28 +6,34 @@ BENCHMARK_DIR = 'benchmarks/'
 
 def parseData(fileContents):
     parts = fileContents.split("\n")
-    keygen = int(parts[1])
-    encsign = int(parts[4])
-    decverify = int(parts[7])
+    # sometimes the output markers end up as the first line of the output file
+    if "=" in parts[0]:
+        keygen = int(parts[2])
+        encsign = int(parts[5])
+        decverify = int(parts[8])
+    else:
+        keygen = int(parts[1])
+        encsign = int(parts[4])
+        decverify = int(parts[7])
     return [keygen, encsign, decverify]
-    
+
 def getStats(l):
-    return "AVG: {:,} <br /> MIN: {:,} <br /> MAX: {:,}".format(int(np.mean(l)), min(l), max(l));    
+    return "AVG: {:,} <br /> MIN: {:,} <br /> MAX: {:,}".format(int(np.mean(l)), min(l), max(l));
 
 def formatData(scheme, implementation, data, printStats):
-    if printStats: 
+    if printStats:
         keygen    = getStats([item[0] for item in data])
         encsign   = getStats([item[1] for item in data])
         decverify = getStats([item[2] for item in data])
         print("| {} ({} executions) | {} | {} |  {} | {} |".format(scheme,len(data), implementation, keygen, encsign, decverify))
-    else:  
+    else:
         keygen     = "{:,}".format(max([item[0] for item in data]))
         encsign    = "{:,}".format(max([item[1] for item in data]))
-        decverify  = "{:,}".format( max([item[2] for item in data])) 
+        decverify  = "{:,}".format( max([item[2] for item in data]))
         print("| {} | {} | {} |  {} | {} |".format(scheme, implementation, keygen, encsign, decverify))
-            
+
 def processPrimitives(path, printStats):
-    if os.path.exists(path) == False: 
+    if os.path.exists(path) == False:
         return;
     for scheme in sorted(os.listdir(path)):
         for implementation in sorted(os.listdir(path+"/"+scheme)):
@@ -37,7 +43,7 @@ def processPrimitives(path, printStats):
                     d = parseData(f.read())
                     measurements.append(d)
             formatData(scheme, implementation, measurements, printStats)
-    
+
 
 print("### Speed Evaluation")
 print("#### Key Encapsulation Schemes")
