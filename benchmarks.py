f5305770078b1f6ca43d9fcd463a57082b46991f
Thom Wiggers <thom@thomwiggers.nl>
2019-03-21 15:09:45 +0000

    Big refactor of python files

--- benchmarks.py
+++ benchmarks.py
@@ -1,54 +1,12 @@
 #!/usr/bin/env python3
-import sys
-import os
-import datetime
-import time
-import utils
+from mupq import mupq
+from interface import M4Settings, M4
 
 
-def benchmarkBinary(benchmark, binary):
-    binpath = os.path.join("bin", binary)
+if __name__ == "__main__":
+    m4 = M4()
+    test = mupq.StackBenchmark(M4Settings(), m4)
+    test.test_all()
 
-    info = binary.split('_')
-    primitive = '_'.join(info[:2])
-    scheme = '_'.join(info[2:-2])
-    implementation = info[-2]
-
-    if utils.m4ignore(primitive, scheme, implementation):
-        return
-
-    if len(sys.argv) > 1 and scheme not in sys.argv[1:]:
-        return
-
-    result = utils.m4run(binpath)
-    if 'ERROR KEYS' in result:
-        print("")
-        print("!!! KEY MISMATCH DURING BENCHMARKING !!!")
-        print("  This could indicate illegal stack usage,")
-        print("  leading to errors when measurement interrupts occur.")
-        print("")
-        print("  .. exiting with errors!")
-        sys.exit(1)
-
-    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d%H%M%S')
-    filename = os.path.join('benchmarks/', benchmark, primitive, scheme, implementation, timestamp)
-    os.makedirs(os.path.dirname(filename), exist_ok=True)
-    with open(filename, 'w') as f:
-        f.write(result.strip())
-    print("  .. wrote benchmarks!")
-
-def doBenchmarks(benchmark):
-    try:
-        binaries = [x for x in os.listdir('bin') if (benchmark+".bin") in x]
-    except FileNotFoundError:
-        print("There is no bin/ folder. Please first make binaries.")
-        sys.exit(1)
-
-    print("This script flashes the benchmarking binaries onto the board, ")
-    print(" and then writes the resulting output to the benchmarks directory.")
-
-    for binary in binaries:
-        benchmarkBinary(benchmark, binary)
-
-doBenchmarks("stack")
-doBenchmarks("speed")
+    test = mupq.SpeedBenchmark(M4Settings(), m4)
+    test.test_all()
--- interface.py
+++ interface.py
@@ -1,69 +1,35 @@
 import subprocess
-import logging
 
 import serial
 
+from mupq import mupq
+
+
+class M4Settings(mupq.PlatformSettings):
+    #: Specify folders to include
+    scheme_folders = [  # mupq.PlatformSettings.scheme_folders + [
+        ('pqm4', 'crypto_kem'),
+        ('pqm4', 'crypto_sign'),
+    ]
+
+    #: List of dicts, in each dict specify (Scheme class) attributes of the
+    #: scheme with values, if all attributes match the scheme is skipped.
+    skip_list = (
+        {'scheme': 'frodo640-aes', 'implementation': 'ref'},
+        {'scheme': 'frodo640-cshake', 'implementation': 'ref'},
+    )
+
+
 dev = serial.Serial("/dev/ttyUSB0", 115200, timeout=10)
 
-class Platform(object):
-    """Implements the M4 platform"""
 
-    def __init__(self, binary_path):
-        self.log = logging.getLogger("m4 interface")
-        self.binary_path = binary_path
+class M4(mupq.Platform):
 
-    def _flash(self):
-        self.log.info("Flashing %s to device", self.binary_path)
-        self.state = 'waiting'
-        self.equals_seen = 0
+    def device(self):
+        return dev
+
+    def flash(self, binary_path):
+        super().flash(binary_path)
         subprocess.check_call(
-            ["st-flash", "--reset", "write", self.binary_path, "0x8000000"],
+            ["st-flash", "--reset", "write", binary_path, "0x8000000"],
         )
-
-    def run(self):
-        """Runs the flashed target and collects the result"""
-        self._flash()
-        self._wait_for_start()
-        self.log.info("Output started")
-        return self._read_output()
-
-    def _wait_for_start(self):
-        """Waits until we read five equals signs"""
-        while self.state == 'waiting':
-            x = dev.read()
-            if x == b'':
-                self.log.warning(
-                    "timed out while waiting for the markers, reflashing")
-                self._flash()
-            elif x == b'=':
-                self.equals_seen += 1
-                continue
-            elif self.equals_seen > 5:
-                self.state = 'beginning'
-                self.log.debug("Found output marker")
-            elif self.equals_seen > 1:
-                logging.warning(
-                    "Got garbage after finding first equals sign, restarting"
-                )
-                self._flash()
-        # Read remaining = signs
-        while self.state == 'beginning':
-            x = dev.read()
-            # Consume remaining =
-            if x != b'=':
-                self.output = [x]
-                self.state = 'reading'
-                break
-
-    def _read_output(self):
-        while self.state == 'reading':
-            x = dev.read()
-            if x == b'#':
-                self.state = 'done'
-                break
-            elif x != b'':
-                self.output.append(x)
-        output = b''.join(self.output).decode('utf-8', 'ignore')
-        # sometimes there's a line full of markers; strip out to avoid errors
-        lines = (x for x in output.split('\n') if not all(c == '=' for c in x))
-        return "{}\n".format('\n'.join(lines))
