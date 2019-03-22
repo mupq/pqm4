0a257aa49dbc22c6cf09548641a11f8f3d691bfb
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-03-22 15:34:59 +0100

    add hashing benchmarks

--- benchmarks.py
+++ benchmarks.py
@@ -5,8 +5,11 @@ from interface import M4Settings, M4
 
 if __name__ == "__main__":
     m4 = M4()
-    test = mupq.StackBenchmark(M4Settings(), m4)
-    test.test_all()
+#    test = mupq.StackBenchmark(M4Settings(), m4)
+#    test.test_all()
+#
+#    test = mupq.SpeedBenchmark(M4Settings(), m4)
+#    test.test_all()
 
-    test = mupq.SpeedBenchmark(M4Settings(), m4)
+    test = mupq.HashingBenchmark(M4Settings(), m4)
     test.test_all()
