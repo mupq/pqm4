2022de19ee3fcb27efcca3bd6134d33132af8fa3
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-03-22 15:38:50 +0100

    benchmarks

--- benchmarks.py
+++ benchmarks.py
@@ -5,11 +5,11 @@ from interface import M4Settings, M4
 
 if __name__ == "__main__":
     m4 = M4()
-#    test = mupq.StackBenchmark(M4Settings(), m4)
-#    test.test_all()
-#
-#    test = mupq.SpeedBenchmark(M4Settings(), m4)
-#    test.test_all()
+    test = mupq.StackBenchmark(M4Settings(), m4)
+    test.test_all()
+
+    test = mupq.SpeedBenchmark(M4Settings(), m4)
+    test.test_all()
 
     test = mupq.HashingBenchmark(M4Settings(), m4)
     test.test_all()
