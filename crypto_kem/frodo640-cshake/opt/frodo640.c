f1345c13368d3f4bf808e6dd97dbdbdbe4dae3e0
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-03-22 10:55:22 +0100

    fix frodo builds

--- crypto_kem/frodo640-cshake/opt/frodo640.c
+++ crypto_kem/frodo640-cshake/opt/frodo640.c
@@ -27,6 +27,6 @@
 uint16_t CDF_TABLE[12] = {4727, 13584, 20864, 26113, 29434, 31278, 32176, 32560, 32704, 32751, 32764, 32767};
 uint16_t CDF_TABLE_LEN = 12;
 
-#include "kem.c"
-#include "noise.c"
-#include "frodo_macrify.c"
+#include "kem.inc"
+#include "noise.inc"
+#include "frodo_macrify.inc"
