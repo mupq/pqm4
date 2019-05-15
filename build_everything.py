13579e454863516dc85f9200f5f842d7f4fe6dac
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-05-15 08:26:56 +0200

    allow filter in build_everything.py

--- build_everything.py
+++ build_everything.py
@@ -2,10 +2,11 @@
 """
 Builds all of the binaries without flashing them.
 """
+import sys
 
 from interface import M4Settings
 from mupq import mupq
 
 
 if __name__ == "__main__":
-    mupq.BuildAll(M4Settings()).test_all()
+    mupq.BuildAll(M4Settings()).test_all(sys.argv[1:])
