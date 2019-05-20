4fef36a9550630ecac37f54d5bd029ac35bdeca0
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-05-20 13:32:30 +0200

    add m4 implementations of round2 newhope

--- crypto_kem/newhope1024cpa/m4/api.h
+++ crypto_kem/newhope1024cpa/m4/api.h
@@ -2,11 +2,11 @@
 #define API_H
 
 
-#define CRYPTO_SECRETKEYBYTES  3680
+#define CRYPTO_SECRETKEYBYTES  1792
 #define CRYPTO_PUBLICKEYBYTES  1824
-#define CRYPTO_CIPHERTEXTBYTES 2208
+#define CRYPTO_CIPHERTEXTBYTES 2176
 #define CRYPTO_BYTES           32
-#define CRYPTO_ALGNAME "NewHope1024-CCAKEM"
+#define CRYPTO_ALGNAME "NewHope1024-CPAKEM"
 
 int crypto_kem_keypair(unsigned char *pk, unsigned char *sk);
 int crypto_kem_enc(unsigned char *ct, unsigned char *ss, const unsigned char *pk);
