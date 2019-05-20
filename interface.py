mupq/pqm4

commit bfb9144fd749e1b77be96755989b9346f5c1908e

Add SPHINCS+ benchmarks (#85)
* Add SPHINCS+ benchmarks

* Add SPHINCS+ to the scheme list in README

* Bring mupq/pqclean up to date for SPHINCS+

This includes MQDSS, which we need to skip

* update mupq

interface.py
@@ -26,6 +26,8 @@ class M4Settings(mupq.PlatformSettings):
         {'scheme': 'frodokem640shake', 'implementation': 'clean'},
         {'scheme': 'frodokem976shake', 'implementation': 'clean'},
         {'scheme': 'frodokem1344shake', 'implementation': 'clean'}
+        {'scheme': 'mqdss-48', 'implementation': 'clean'}
+        {'scheme': 'mqdss-64', 'implementation': 'clean'}
     )
 
 

commit ab68fb4956c25c3f10b5178202cdfef6fc41d6da

Fix interface.py
Commit bfb9144fd749e1b77be96755989b9346f5c1908e introduced a syntax
error. For me it works again now.

interface.py
@@ -25,9 +25,9 @@ class M4Settings(mupq.PlatformSettings):
         {'scheme': 'frodokem1344aes', 'implementation': 'clean'},
         {'scheme': 'frodokem640shake', 'implementation': 'clean'},
         {'scheme': 'frodokem976shake', 'implementation': 'clean'},
-        {'scheme': 'frodokem1344shake', 'implementation': 'clean'}
-        {'scheme': 'mqdss-48', 'implementation': 'clean'}
-        {'scheme': 'mqdss-64', 'implementation': 'clean'}
+        {'scheme': 'frodokem1344shake', 'implementation': 'clean'},
+        {'scheme': 'mqdss-48', 'implementation': 'clean'},
+        {'scheme': 'mqdss-64', 'implementation': 'clean'},
     )
 
 