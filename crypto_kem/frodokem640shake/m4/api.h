9a12821c9f127f1ddd03b9be6066f1ec3c52dd23
Matthias J. Kannwischer <matthias@kannwischer.eu>
2019-06-12 16:08:30 +0200

    optimized frodokem640shake round 2

---
+++ crypto_kem/frodokem640shake/m4/api.h
@@ -0,0 +1,20 @@
+#ifndef API_H
+#define API_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define CRYPTO_SECRETKEYBYTES  19888     // sizeof(s) + CRYPTO_PUBLICKEYBYTES + 2*PARAMS_N*PARAMS_NBAR + BYTES_PKHASH
+#define CRYPTO_PUBLICKEYBYTES   9616     // sizeof(seed_A) + (PARAMS_LOGQ*PARAMS_N*PARAMS_NBAR)/8
+#define CRYPTO_BYTES              16
+#define CRYPTO_CIPHERTEXTBYTES  9720     // (PARAMS_LOGQ*PARAMS_N*PARAMS_NBAR)/8 + (PARAMS_LOGQ*PARAMS_NBAR*PARAMS_NBAR)/8
+
+#define CRYPTO_ALGNAME "FrodoKEM-640-SHAKE"
+
+int crypto_kem_keypair(uint8_t *pk, uint8_t *sk);
+
+int crypto_kem_enc(uint8_t *ct, uint8_t *ss, const uint8_t *pk);
+
+int crypto_kem_dec(uint8_t *ss, const uint8_t *ct, const uint8_t *sk);
+
+#endif
