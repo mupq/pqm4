31a213b026999d9089eeeffd56389ebd05a9c419
PRASANNA-RAVI <52766028+PRASANNA-RAVI@users.noreply.github.com>
2019-08-05 12:15:34 +0800

    Add files via upload

---
+++ crypto_sign/dilithium4/opt/rounding.h
@@ -0,0 +1,11 @@
+#ifndef ROUNDING_H
+#define ROUNDING_H
+
+#include <stdint.h>
+
+uint32_t power2round(uint32_t a, uint32_t *a0);
+uint32_t decompose(uint32_t a, uint32_t *a0);
+unsigned int make_hint(uint32_t a0, uint32_t a1);
+uint32_t use_hint(uint32_t a, unsigned int hint);
+
+#endif
