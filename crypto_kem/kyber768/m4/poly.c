dcb73d2071bf801e7635932e3a44ecabed7b6e55
Alper Bilgin <y.alperbilgin@gmail.com>
2020-01-09 22:53:40 +0300

    An additional Barrett reduction is added to ensure that all coefficients of both public and secret keys are in [0, q-1]. Without this new reduction they are in [0, q].

--- crypto_kem/kyber768/m4/poly.c
+++ crypto_kem/kyber768/m4/poly.c
@@ -326,6 +326,8 @@ void poly_tobytes(unsigned char *r, poly *a) {
     int i;
     uint16_t t0, t1;
 
+    poly_reduce(a);
+
     for (i = 0; i < KYBER_N / 2; i++) {
         t0 = a->coeffs[2 * i];
         t1 = a->coeffs[2 * i + 1];
