09726167f3f0907a5e822e99a41e3271bbff3ff4
Matthias J. Kannwischer <matthias@kannwischer.eu>
2020-02-15 10:41:15 +0100

    Fixes #132 (#133)

--- crypto_kem/newhope512cca/m4/verify.c
+++ crypto_kem/newhope512cca/m4/verify.c
@@ -3,30 +3,6 @@
 #include <string.h>
 
 /*************************************************
-* Name:        verify
-*
-* Description: Compare two arrays for equality in constant time.
-*
-* Arguments:   const unsigned char *a: pointer to first byte array
-*              const unsigned char *b: pointer to second byte array
-*              size_t len:             length of the byte arrays
-*
-* Returns 0 if the byte arrays are equal, 1 otherwise
-**************************************************/
-int verify(const unsigned char *a, const unsigned char *b, size_t len) {
-  uint64_t r;
-  size_t i;
-  r = 0;
-
-  for (i = 0; i < len; i++) {
-    r |= a[i] ^ b[i];
-  }
-
-  r = (-(int64_t)r) >> 63;
-  return (int)r;
-}
-
-/*************************************************
 * Name:        cmov
 *
 * Description: Copy len bytes from x to r if b is 1;
