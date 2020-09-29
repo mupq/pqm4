4975d4d7db0b97607752e9de6593ee0b11a7eedb
Peter Schwabe <peter@cryptojedi.org>
2020-09-29 11:33:33 +0200

    Integrated changes to noise sampling in Kyber512

--- crypto_kem/kyber512/m4/cbd.c
+++ crypto_kem/kyber512/m4/cbd.c
@@ -1,1 +1,112 @@
-../../kyber768/m4/cbd.c
+#include "cbd.h"
+#include "params.h"
+
+#include <stdint.h>
+
+/*************************************************
+* Name:        load32_littleendian
+*
+* Description: load bytes into a 32-bit integer
+*              in little-endian order
+*
+* Arguments:   - const unsigned char *x: pointer to input byte array
+*
+* Returns 32-bit unsigned integer loaded from x
+**************************************************/
+static uint32_t load32_littleendian(const unsigned char *x) {
+    uint32_t r;
+    r  = (uint32_t)x[0];
+    r |= (uint32_t)x[1] << 8;
+    r |= (uint32_t)x[2] << 16;
+    r |= (uint32_t)x[3] << 24;
+    return r;
+}
+
+/*************************************************
+* Name:        load24_littleendian
+*
+* Description: load 3 bytes into a 32-bit integer
+*              in little-endian order
+*              This function is only needed for Kyber-512
+*
+* Arguments:   - const uint8_t *x: pointer to input byte array
+*
+* Returns 32-bit unsigned integer loaded from x (most significant byte is zero)
+**************************************************/
+static uint32_t load24_littleendian(const uint8_t x[3])
+{
+  uint32_t r;
+  r  = (uint32_t)x[0];
+  r |= (uint32_t)x[1] << 8;
+  r |= (uint32_t)x[2] << 16;
+  return r;
+}
+
+
+
+/*************************************************
+* Name:        cbd_eta1
+*
+* Description: Given an array of uniformly random bytes, compute
+*              polynomial with coefficients distributed according to
+*              a centered binomial distribution with parameter KYBER_ETA1
+*              specialized for KYBER_ETA1=3
+*
+* Arguments:   - poly *r:                  pointer to output polynomial
+*              - const unsigned char *buf: pointer to input byte array
+*              - int add:                  boolean to indicate to accumulate into r
+**************************************************/
+void cbd_eta1(poly *r, const unsigned char *buf, int add) {
+  unsigned int i,j;
+  uint32_t t,d;
+  int16_t a,b;
+
+  for(i=0;i<KYBER_N/4;i++) {
+    t  = load24_littleendian(buf+3*i);
+    d  = t & 0x00249249;
+    d += (t>>1) & 0x00249249;
+    d += (t>>2) & 0x00249249;
+
+    for(j=0;j<4;j++) {
+      a = (d >> (6*j+0)) & 0x7;
+      b = (d >> (6*j+3)) & 0x7;
+      if (!add)
+        r->coeffs[4 * i + j] = 0;
+      r->coeffs[4 * i + j] = r->coeffs[4 * i + j] + (a - b);
+    }
+  }
+}
+
+/*************************************************
+* Name:        cbd_eta2
+*
+* Description: Given an array of uniformly random bytes, compute
+*              polynomial with coefficients distributed according to
+*              a centered binomial distribution with parameter KYBER_ETA2
+*              specialized for KYBER_ETA2=2
+*
+* Arguments:   - poly *r:                  pointer to output polynomial
+*              - const unsigned char *buf: pointer to input byte array
+*              - int add:                  boolean to indicate to accumulate into r
+**************************************************/
+void cbd_eta2(poly *r, const unsigned char *buf, int add) {
+    uint32_t d, t;
+    int16_t a, b;
+    int i, j;
+
+    for (i = 0; i < KYBER_N / 8; i++) {
+        t = load32_littleendian(buf + 4 * i);
+        d  = t & 0x55555555;
+        d += (t >> 1) & 0x55555555;
+
+        for (j = 0; j < 8; j++) {
+            a = (d >>  4 * j)    & 0x3;
+            b = (d >> (4 * j + 2)) & 0x3;
+            if (!add)
+              r->coeffs[8 * i + j] = 0;
+            r->coeffs[8 * i + j] = r->coeffs[8 * i + j] + (a - b);
+        }
+    }
+}
+
+
--- crypto_kem/kyber512/m4/indcpa.c
+++ crypto_kem/kyber512/m4/indcpa.c
@@ -1,1 +1,276 @@
-../../kyber768/m4/indcpa.c
+#include "indcpa.h"
+#include "ntt.h"
+#include "poly.h"
+#include "polyvec.h"
+#include "randombytes.h"
+#include "symmetric.h"
+
+#include <string.h>
+#include <stdint.h>
+
+extern void doublebasemul_asm_acc(int16_t *r, const int16_t *a, const int16_t *b, int16_t zeta);
+/*************************************************
+* Name:        matacc
+*
+* Description: Multiplies a row of A or A^T, generated on-the-fly,
+*              with a vector of polynomials and accumulates into the result.
+*
+* Arguments:   - poly *r:                    pointer to output polynomial to accumulate in
+*              - polyvec *b:                 pointer to input vector of polynomials to multiply with
+*              - unsigned char i:            byte to indicate the index < KYBER_K of the row of A or A^T
+*              - const unsigned char *seed:  pointer to the public seed used to generate A
+*              - int transposed:             boolean indicatin whether A or A^T is generated
+**************************************************/
+static void matacc(poly* r, polyvec *b, unsigned char i, const unsigned char *seed, int transposed) {
+  unsigned char buf[XOF_BLOCKBYTES+2];
+  unsigned int buflen, off;
+  xof_state state;
+  unsigned int ctr, pos, k, l;
+  uint16_t val0, val1;
+  int16_t c[4];
+
+  poly_zeroize(r);
+
+  for(int j=0;j<KYBER_K;j++) {
+    ctr = pos = 0;
+    if (transposed)
+      xof_absorb(&state, seed, i, j);
+    else
+      xof_absorb(&state, seed, j, i);
+
+    xof_squeezeblocks(buf, 1, &state);
+    buflen = XOF_BLOCKBYTES;
+
+    k = 0;
+    while (ctr < KYBER_N/4)
+    {
+      val0 = ((buf[pos+0] >> 0) | ((uint16_t)buf[pos+1] << 8)) & 0xFFF;
+      val1 = ((buf[pos+1] >> 4) | ((uint16_t)buf[pos+2] << 4)) & 0xFFF;
+      pos += 3;
+
+      if (val0 < KYBER_Q) {
+        c[k++] = (int16_t) val0;
+        if (k == 4) {
+          doublebasemul_asm_acc(&r->coeffs[4*ctr], &b->vec[j].coeffs[4*ctr], c, zetas[ctr]);
+          ctr++;
+          k = 0;
+        }
+      }
+
+      if (val1 < KYBER_Q && ctr < KYBER_Q/4) {
+        c[k++] = (int16_t) val1;
+        if (k == 4) {
+          doublebasemul_asm_acc(&r->coeffs[4*ctr], &b->vec[j].coeffs[4*ctr], c, zetas[ctr]);
+          ctr++;
+          k = 0;
+        }
+      }
+
+      if (pos + 3 > buflen && ctr < KYBER_Q/4) {
+        off = buflen % 3;
+        for(l = 0; l < off; l++)
+          buf[l] = buf[buflen - off + l];
+        xof_squeezeblocks(buf + off, 1, &state);
+        buflen = off + XOF_BLOCKBYTES;
+        pos = 0;
+      }
+    }
+  }
+}
+
+/*************************************************
+* Name:        indcpa_keypair
+*
+* Description: Generates public and private key for the CPA-secure
+*              public-key encryption scheme underlying Kyber
+*
+* Arguments:   - unsigned char *pk: pointer to output public key (of length KYBER_INDCPA_PUBLICKEYBYTES bytes)
+*              - unsigned char *sk: pointer to output private key (of length KYBER_INDCPA_SECRETKEYBYTES bytes)
+**************************************************/
+void indcpa_keypair(unsigned char *pk, unsigned char *sk) {
+    polyvec skpv;
+    poly pkp;
+    unsigned char buf[2 * KYBER_SYMBYTES];
+    unsigned char *publicseed = buf;
+    unsigned char *noiseseed = buf + KYBER_SYMBYTES;
+    int i;
+    unsigned char nonce = 0;
+
+    randombytes(buf, KYBER_SYMBYTES);
+    hash_g(buf, buf, KYBER_SYMBYTES);
+
+    for (i = 0; i < KYBER_K; i++)
+        poly_getnoise_eta1(skpv.vec + i, noiseseed, nonce++);
+
+    polyvec_ntt(&skpv);
+
+    for (i = 0; i < KYBER_K; i++) {
+        matacc(&pkp, &skpv, i, publicseed, 0);
+        poly_invntt(&pkp);
+
+        poly_addnoise_eta1(&pkp, noiseseed, nonce++);
+        poly_ntt(&pkp);
+
+        poly_tobytes(pk+i*KYBER_POLYBYTES, &pkp);
+    }
+
+    polyvec_tobytes(sk, &skpv);
+    memcpy(pk + KYBER_POLYVECBYTES, publicseed, KYBER_SYMBYTES); // Pack the public seed in the public key
+}
+
+/*************************************************
+* Name:        indcpa_enc
+*
+* Description: Encryption function of the CPA-secure
+*              public-key encryption scheme underlying Kyber.
+*
+* Arguments:   - unsigned char *c:          pointer to output ciphertext (of length KYBER_INDCPA_BYTES bytes)
+*              - const unsigned char *m:    pointer to input message (of length KYBER_INDCPA_MSGBYTES bytes)
+*              - const unsigned char *pk:   pointer to input public key (of length KYBER_INDCPA_PUBLICKEYBYTES bytes)
+*              - const unsigned char *coin: pointer to input random coins used as seed (of length KYBER_SYMBYTES bytes)
+*                                           to deterministically generate all randomness
+**************************************************/
+void indcpa_enc(unsigned char *c,
+               const unsigned char *m,
+               const unsigned char *pk,
+               const unsigned char *coins) {
+    polyvec sp;
+    poly bp;
+    poly *pkp = &bp;
+    poly *k = &bp;
+    poly *v = &sp.vec[0];
+    const unsigned char *seed = pk+KYBER_POLYVECBYTES;
+    int i;
+    unsigned char nonce = 0;
+
+    for (i = 0; i < KYBER_K; i++)
+        poly_getnoise_eta1(sp.vec + i, coins, nonce++);
+
+    polyvec_ntt(&sp);
+
+    for (i = 0; i < KYBER_K; i++) {
+        matacc(&bp, &sp, i, seed, 1);
+        poly_invntt(&bp);
+
+        poly_addnoise_eta2(&bp, coins, nonce++);
+        poly_reduce(&bp);
+
+        poly_packcompress(c, &bp, i);
+    }
+
+    poly_frombytes(pkp, pk);
+    poly_basemul(v, pkp, &sp.vec[0]);
+    for (i = 1; i < KYBER_K; i++) {
+        poly_frombytes(pkp, pk + i*KYBER_POLYBYTES);
+        poly_basemul_acc(v, pkp, &sp.vec[i]);
+    }
+
+    poly_invntt(v);
+
+    poly_addnoise_eta2(v, coins, nonce++);
+
+    poly_frommsg(k, m);
+    poly_add(v, v, k);
+    poly_reduce(v);
+
+    poly_compress(c + KYBER_POLYVECCOMPRESSEDBYTES, v);
+}
+
+/*************************************************
+* Name:        indcpa_enc_cmp
+*
+* Description: Re-encryption function.
+*              Compares the re-encypted ciphertext with the original ciphertext byte per byte.
+*              The comparison is performed in a constant time manner.
+*
+*
+* Arguments:   - unsigned char *ct:         pointer to input ciphertext to compare the new ciphertext with (of length KYBER_INDCPA_BYTES bytes)
+*              - const unsigned char *m:    pointer to input message (of length KYBER_INDCPA_MSGBYTES bytes)
+*              - const unsigned char *pk:   pointer to input public key (of length KYBER_INDCPA_PUBLICKEYBYTES bytes)
+*              - const unsigned char *coin: pointer to input random coins used as seed (of length KYBER_SYMBYTES bytes)
+*                                           to deterministically generate all randomness
+* Returns:     - boolean byte indicating that re-encrypted ciphertext is NOT equal to the original ciphertext
+**************************************************/
+unsigned char indcpa_enc_cmp(const unsigned char *c,
+                             const unsigned char *m,
+                             const unsigned char *pk,
+                             const unsigned char *coins) {
+    uint64_t rc = 0;
+    polyvec sp;
+    poly bp;
+    poly *pkp = &bp;
+    poly *k = &bp;
+    poly *v = &sp.vec[0];
+    const unsigned char *seed = pk+KYBER_POLYVECBYTES;
+    int i;
+    unsigned char nonce = 0;
+
+    for (i = 0; i < KYBER_K; i++)
+        poly_getnoise_eta1(sp.vec + i, coins, nonce++);
+
+    polyvec_ntt(&sp);
+
+    for (i = 0; i < KYBER_K; i++) {
+        matacc(&bp, &sp, i, seed, 1);
+        poly_invntt(&bp);
+
+        poly_addnoise_eta2(&bp, coins, nonce++);
+        poly_reduce(&bp);
+
+        rc |= cmp_poly_packcompress(c, &bp, i);
+    }
+
+    poly_frombytes(pkp, pk);
+    poly_basemul(v, pkp, &sp.vec[0]);
+    for (i = 1; i < KYBER_K; i++) {
+        poly_frombytes(pkp, pk + i*KYBER_POLYBYTES);
+        poly_basemul_acc(v, pkp, &sp.vec[i]);
+    }
+
+    poly_invntt(v);
+
+    poly_addnoise_eta2(v, coins, nonce++);
+    poly_frommsg(k, m);
+    poly_add(v, v, k);
+    poly_reduce(v);
+
+    rc |= cmp_poly_compress(c + KYBER_POLYVECCOMPRESSEDBYTES, v);
+
+    rc = ~rc + 1;
+    rc >>= 63;
+    return (unsigned char)rc;
+}
+
+/*************************************************
+* Name:        indcpa_dec
+*
+* Description: Decryption function of the CPA-secure
+*              public-key encryption scheme underlying Kyber.
+*
+* Arguments:   - unsigned char *m:        pointer to output decrypted message (of length KYBER_INDCPA_MSGBYTES)
+*              - const unsigned char *c:  pointer to input ciphertext (of length KYBER_INDCPA_BYTES)
+*              - const unsigned char *sk: pointer to input secret key (of length KYBER_INDCPA_SECRETKEYBYTES)
+**************************************************/
+void __attribute__ ((noinline)) indcpa_dec(unsigned char *m,
+                                           const unsigned char *c,
+                                           const unsigned char *sk) {
+    poly mp, bp;
+    poly *v = &bp;
+
+    poly_unpackdecompress(&mp, c, 0);
+    poly_ntt(&mp);
+    poly_frombytes_mul(&mp, sk);
+    for(int i = 1; i < KYBER_K; i++) {
+        poly_unpackdecompress(&bp, c, i);
+        poly_ntt(&bp);
+        poly_frombytes_mul(&bp, sk + i*KYBER_POLYBYTES);
+        poly_add(&mp, &mp, &bp);
+    }
+
+    poly_invntt(&mp);
+    poly_decompress(v, c+KYBER_POLYVECCOMPRESSEDBYTES);
+    poly_sub(&mp, v, &mp);
+    poly_reduce(&mp);
+
+    poly_tomsg(m, &mp);
+}
