06700ece2ca42df141fe54971fcbd516fdaba2ae
Richard Petri <git@rpls.de>
2021-01-17 01:50:24 +0100

    Fix argument parsing for benchmarks

--- benchmarks.py
+++ benchmarks.py
@@ -6,18 +6,22 @@ if __name__ == "__main__":
     args, rest = parse_arguments()
     platform, settings = get_platform(args)
     with platform:
+        schemes = [s for s in rest if s not in ['--nostack',
+                                                '--nospeed',
+                                                '--nohashing',
+                                                '--nosize']]
         if "--nostack" not in rest:
             test = mupq.StackBenchmark(settings, platform)
-            test.test_all(rest)
+            test.test_all(schemes)
 
         if "--nospeed" not in rest:
             test = mupq.SpeedBenchmark(settings, platform)
-            test.test_all(rest)
+            test.test_all(schemes)
 
         if "--nohashing" not in rest:
             test = mupq.HashingBenchmark(settings, platform)
-            test.test_all(rest)
+            test.test_all(schemes)
 
         if "--nosize" not in rest:
             test = mupq.SizeBenchmark(settings, platform)
-            test.test_all(rest)
+            test.test_all(schemes)
