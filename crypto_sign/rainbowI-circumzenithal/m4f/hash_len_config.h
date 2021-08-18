b2c37fd3908bd1a740a228b64cec8fe951319060
Matthias J. Kannwischer <matthias@kannwischer.eu>
2021-08-18 17:08:18 +0800

    switch to symlinks for files that are shared

--- crypto_sign/rainbowI-circumzenithal/m4f/hash_len_config.h
+++ crypto_sign/rainbowI-circumzenithal/m4f/hash_len_config.h
@@ -1,16 +1,1 @@
-/// @file hash_len_config.h
-/// @brief defining the lenght of outputs of the internel hash functions.
-///
-
-#ifndef _HASH_LEN_CONFIG_H_
-#define _HASH_LEN_CONFIG_H_
-
-
-#include "rainbow_config.h"
-
-/// defining the lenght of outputs of the internel hash functions.
-#ifndef _HASH_LEN
-#define _HASH_LEN (32)
-#endif
-
-#endif
+../../rainbowI-classic/m4f/hash_len_config.h
