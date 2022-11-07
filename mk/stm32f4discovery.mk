6c17f2bfef737f3099b3ebe73b65a9f6dd5f3b26
Matthias J. Kannwischer <matthias@kannwischer.eu>
2022-11-07 17:29:20 +0800

    clean up excluded schemes

--- mk/stm32f4discovery.mk
+++ mk/stm32f4discovery.mk
@@ -2,8 +2,7 @@ DEVICE=stm32f407vg
 
 EXCLUDED_SCHEMES = \
 	mupq/pqclean/crypto_kem/mceliece% \
-	mupq/crypto_sign/falcon-1024-tree% \
-	mupq/pqclean/crypto_sign/rainbow%
+	mupq/crypto_sign/falcon-1024-tree%
 
 include mk/opencm3.mk
 
