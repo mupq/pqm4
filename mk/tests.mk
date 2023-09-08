37cd04ca6ef404e59a9dd089b400f81ac2dfb868
Richard Petri <git@rpls.de>
2023-09-08 22:50:12 +0200

    Add a simple Keccak test

--- mk/tests.mk
+++ mk/tests.mk
@@ -4,12 +4,16 @@ elf/boardtest.elf: common/test.c $(LINKDEPS) $(CONFIG)
 
 elf/aestest.elf: common/aestest.c $(LINKDEPS) $(CONFIG)
 	$(compiletest)
+
+elf/keccaktest.elf: common/keccaktest.c $(LINKDEPS) $(CONFIG)
+	$(compiletest)
 else
 elf/boardtest.elf: $(call objs,common/test.c) $(LINKDEPS) $(CONFIG)
 
 elf/aestest.elf: $(call objs,common/aestest.c) $(LINKDEPS) $(CONFIG)
+
+elf/keccaktest.elf: $(call objs,common/keccaktest.c) $(LINKDEPS) $(CONFIG)
 endif
 
-
-tests: elf/boardtest.elf elf/aestest.elf
-tests-bin: bin/boardtest.bin bin/aestest.bin
+tests: elf/boardtest.elf elf/aestest.elf elf/keccaktest.elf
+tests-bin: bin/boardtest.bin bin/aestest.bin bin/keccaktest.elf
