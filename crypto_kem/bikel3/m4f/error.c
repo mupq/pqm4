6ea87b6e8b3a67e85270b878d85c998aad22b30f
Till Eifert <t.eifert@uni-luebeck.de>
2023-10-26 17:22:43 +0200

    BIKE remove outdated files (aes) and use symlinks for duplicate files between bike level 1 and bike level 3

--- crypto_kem/bikel3/m4f/error.c
+++ crypto_kem/bikel3/m4f/error.c
@@ -1,14 +1,1 @@
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0"
- *
- * Written by Nir Drucker, Shay Gueron and Dusan Kostic,
- * AWS Cryptographic Algorithms Group.
- *
- * Modification: 2021 Ming-Shing Chen, Tung Chou, and Markus Krausz
- *
- */
-
-#include "error.h"
-
-//__thread _bike_err_t bike_errno;
-_bike_err_t bike_errno;
+../../bikel1/m4f/error.c
