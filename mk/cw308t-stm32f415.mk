b381da5beba2fab7725b7da5a8aca56a49906eba
Richard Petri <git@rpls.de>
2024-01-16 22:06:29 +0100

    Exclude all non-building schemes for cw308t-stm32f415

--- mk/cw308t-stm32f415.mk
+++ mk/cw308t-stm32f415.mk
@@ -2,13 +2,23 @@ DEVICE=stm32f415rgt6
 OPENCM3_TARGET=lib/stm32/f4
 
 EXCLUDED_SCHEMES = \
+	mupq/pqclean/crypto_kem/mceliece% \
 	mupq/pqclean/crypto_sign/sphincs-haraka-256f% \
 	mupq/pqclean/crypto_sign/sphincs-shake256-256f% \
 	mupq/pqclean/crypto_sign/sphincs-sha256-256f% \
-	mupq/pqclean/crypto_kem/mceliece% \
+	mupq/crypto_sign/ov-Ip% \
+	crypto_sign/ov-Ip% \
 	mupq/crypto_sign/falcon-1024% \
 	mupq/crypto_sign/falcon-512% \
 	crypto_sign/falcon-1024% \
-	crypto_sign/falcon-512%
+	crypto_sign/falcon-512% \
+	mupq/crypto_sign/meds167717% \
+	mupq/crypto_sign/meds134180% \
+	mupq/crypto_sign/snova-60-10-16-4% \
+	mupq/crypto_sign/snova-49-11-16-3% \
+	mupq/crypto_sign/snova-37-8-16-4% \
+	mupq/crypto_sign/snova-66-15-16-3% \
+	mupq/crypto_sign/snova-43-25-16-2% \
+	mupq/crypto_sign/snova-61-33-16-2%
 
 include mk/opencm3.mk
