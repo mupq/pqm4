280423741d94e335182059c122272508758a9edf
Amin Abdulrahman <amin@abdulrahman.de>
2024-03-18 14:02:58 +0100

    rm 257 FFT

--- crypto_sign/dilithium3/m4fstack/smallpoly.h
+++ crypto_sign/dilithium3/m4fstack/smallpoly.h
@@ -6,25 +6,12 @@
 
 
 
-#if DILITHIUM_MODE == 3 // use q=769
 #define SMALL_POLY_16_BIT
 typedef struct {
     int16_t coeffs[N];
 } smallpoly;
 
 typedef smallpoly smallhalfpoly;
-
-#else // use q=257
-#define SMALL_POLY_32_BIT
-typedef struct {
-    int32_t coeffs[N];
-} smallpoly;
-
-typedef struct {
-    int16_t coeffs[N];
-} smallhalfpoly;
-#endif
-
 
 void poly_small_ntt_precomp(smallpoly *out, smallhalfpoly *out2, poly *in);
 void polyvecl_small_ntt(smallpoly v[L]);
