c33fab9dc0498f9a6583c509192b26759b3694cd
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-03-26 09:56:42 -0400

    fix nucleo-l476rg build

--- mk/nucleo-l476rg.mk
+++ mk/nucleo-l476rg.mk
@@ -3,7 +3,18 @@ OPENCM3_TARGET=lib/stm32/l4
 
 EXCLUDED_SCHEMES = \
 	mupq/pqclean/crypto_kem/mceliece% \
+	mupq/crypto_sign/ov-Ip% \
 	mupq/crypto_sign/falcon-1024-tree% \
-	crypto_sign/ov-Ip%
-
+	mupq/crypto_sign/snova-43-25-16-2-esk% \
+	mupq/crypto_sign/snova-61-33-16-2-esk% \
+	mupq/crypto_sign/snova-60-10-16-4-esk% \
+	mupq/crypto_sign/snova-66-15-16-3-esk% \
+	mupq/crypto_sign/snova-49-11-16-3-esk% \
+	mupq/crypto_sign/snova-37-8-16-4-esk% \
+	mupq/crypto_sign/meds55604% \
+	mupq/crypto_sign/meds167717% \
+	mupq/crypto_sign/meds134180% \
+	mupq/crypto_sign/tuov% \
+	crypto_sign/ov-Ip% \
+	
 include mk/opencm3.mk
