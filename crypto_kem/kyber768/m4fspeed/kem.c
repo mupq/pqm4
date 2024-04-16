4584cfcef04e937c92ca6a923c9ea42e01d3122e
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-04-16 09:24:06 +0800

    Fix inlining of Kyber re-encryption (#339)
    * fix inlining of Kyber reencryption
    
    * update benchmarks

--- crypto_kem/kyber768/m4fspeed/kem.c
+++ crypto_kem/kyber768/m4fspeed/kem.c
@@ -138,7 +138,6 @@ int crypto_kem_dec(unsigned char *ss, const unsigned char *ct, const unsigned ch
     uint8_t buf[2 * KYBER_SYMBYTES];
     /* Will contain key, coins */
     uint8_t kr[2 * KYBER_SYMBYTES];
-    uint8_t cmp[KYBER_CIPHERTEXTBYTES + KYBER_SYMBYTES];
     const uint8_t *pk = sk + KYBER_INDCPA_SECRETKEYBYTES;
 
     indcpa_dec(buf, ct, sk);
@@ -148,9 +147,7 @@ int crypto_kem_dec(unsigned char *ss, const unsigned char *ct, const unsigned ch
     hash_g(kr, buf, 2 * KYBER_SYMBYTES);
 
     /* coins are in kr+KYBER_SYMBYTES */
-    indcpa_enc(cmp, buf, pk, kr + KYBER_SYMBYTES);
-
-    fail = verify(ct, cmp, KYBER_CIPHERTEXTBYTES);
+    fail = indcpa_enc_cmp(ct, buf, pk, kr + KYBER_SYMBYTES);
 
     /* Compute rejection key */
     rkprf(ss, sk + KYBER_SECRETKEYBYTES - KYBER_SYMBYTES, ct);
