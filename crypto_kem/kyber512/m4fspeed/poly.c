7642fe16c36c41e166e2d61160b7aaa217ff309f
Matthias J. Kannwischer <matthias@kannwischer.eu>
2024-06-11 16:32:41 +0800

    Fixed secret-dependent branch in poly_fromsg

--- crypto_kem/kyber512/m4fspeed/poly.c
+++ crypto_kem/kyber512/m4fspeed/poly.c
@@ -614,16 +614,20 @@ void poly_sub(poly *r, const poly *a, const poly *b) {
 * Arguments:   - poly *r:                  pointer to output polynomial
 *              - const unsigned char *msg: pointer to input message
 **************************************************/
-void poly_frommsg(poly *r, const unsigned char msg[KYBER_SYMBYTES]) {
-    int i, j;
-    uint16_t mask;
+void poly_frommsg(poly *r, const uint8_t msg[KYBER_INDCPA_MSGBYTES])
+{
+  unsigned int i,j;
 
-    for (i = 0; i < KYBER_SYMBYTES; i++) {
-        for (j = 0; j < 8; j++) {
-            mask = -((msg[i] >> j) & 1);
-            r->coeffs[8 * i + j] = mask & ((KYBER_Q + 1) / 2);
-        }
+#if (KYBER_INDCPA_MSGBYTES != KYBER_N/8)
+#error "KYBER_INDCPA_MSGBYTES must be equal to KYBER_N/8 bytes!"
+#endif
+
+  for(i=0;i<KYBER_N/8;i++) {
+    for(j=0;j<8;j++) {
+      r->coeffs[8*i+j] = 0;
+      cmov_int16(r->coeffs+8*i+j, ((KYBER_Q+1)/2), (msg[i] >> j)&1);
     }
+  }
 }
 
 /*************************************************
--- crypto_kem/kyber768/m4fstack/poly.c
+++ crypto_kem/kyber768/m4fstack/poly.c
@@ -552,6 +552,8 @@ void poly_sub(poly *r, const poly *a, const poly *b) {
     pointwise_sub(r->coeffs,a->coeffs,b->coeffs);
 }
 
+void cmov_int16(int16_t *r, int16_t v, uint16_t b);
+
 /*************************************************
 * Name:        poly_frommsg
 *
@@ -560,16 +562,20 @@ void poly_sub(poly *r, const poly *a, const poly *b) {
 * Arguments:   - poly *r:                  pointer to output polynomial
 *              - const unsigned char *msg: pointer to input message
 **************************************************/
-void poly_frommsg(poly *r, const unsigned char msg[KYBER_SYMBYTES]) {
-    int i, j;
-    uint16_t mask;
+void poly_frommsg(poly *r, const uint8_t msg[KYBER_INDCPA_MSGBYTES])
+{
+  unsigned int i,j;
 
-    for (i = 0; i < KYBER_SYMBYTES; i++) {
-        for (j = 0; j < 8; j++) {
-            mask = -((msg[i] >> j) & 1);
-            r->coeffs[8 * i + j] = mask & ((KYBER_Q + 1) / 2);
-        }
+#if (KYBER_INDCPA_MSGBYTES != KYBER_N/8)
+#error "KYBER_INDCPA_MSGBYTES must be equal to KYBER_N/8 bytes!"
+#endif
+
+  for(i=0;i<KYBER_N/8;i++) {
+    for(j=0;j<8;j++) {
+      r->coeffs[8*i+j] = 0;
+      cmov_int16(r->coeffs+8*i+j, ((KYBER_Q+1)/2), (msg[i] >> j)&1);
     }
+  }
 }
 
 /*************************************************
