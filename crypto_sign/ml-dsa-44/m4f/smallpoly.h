271ca71a7ea627c16f3983891393eea910a6da00
Matthias J. Kannwischer <matthias@kannwischer.eu>
2025-02-10 12:56:46 +0800

    ML-DSA-44/87: Switch to mod 769 NTT instead  of mod 257 FTT
    In https://tches.iacr.org/index.php/TCHES/article/view/11419 it was shown
    that Plantard-based NTTs mod 769 are faster than the 257 Fermat Number
    Transforms that we are currently using for ML-DSA-44+87.
    This commit switches to the mod 769 arthmetic for all parameter sets instead
    of only using it for ML-DSA-65.
    
    Files are in ML-DSA-44 and symlinked from 65 + 87.
    Stack-optimized implementations remain unaffected (they were using 769
    before).

--- crypto_sign/ml-dsa-44/m4f/smallpoly.h
+++ crypto_sign/ml-dsa-44/m4f/smallpoly.h
@@ -6,7 +6,6 @@
 
 
 
-#if DILITHIUM_MODE == 3 // use q=769
 #define SMALL_POLY_16_BIT
 typedef struct {
     int16_t coeffs[N];
@@ -14,16 +13,6 @@ typedef struct {
 
 typedef smallpoly smallhalfpoly;
 
-#else // use q=257
-#define SMALL_POLY_32_BIT
-typedef struct {
-    int32_t coeffs[N];
-} smallpoly;
-
-typedef struct {
-    int16_t coeffs[N];
-} smallhalfpoly;
-#endif
 
 
 void poly_small_ntt_precomp(smallpoly *out, smallhalfpoly *out2, poly *in);
