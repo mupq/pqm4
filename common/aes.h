06b4c552550c29a0c9dbd8d7f4ac4b18d23765b4
Shang-Yi Yang <ilway25@gmail.com>
2025-05-08 14:51:27 +0800

    revert accidental AES changes

--- common/aes.h
+++ common/aes.h
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0 or CC0-1.0
 #ifndef AES_H
 #define AES_H
 
@@ -28,7 +29,7 @@ void aes128_ctr_keyexp(aes128ctx *r, const unsigned char *key);
 
 void aes128_ecb(unsigned char *out, const unsigned char *in, size_t nblocks, const aes128ctx *ctx);
 
-void aes128_ctr(unsigned char *out, size_t outlen, const unsigned char *iv, uint32_t ctr, const aes128ctx *ctx);
+void aes128_ctr(unsigned char *out, size_t outlen, const unsigned char *iv, const aes128ctx *ctx);
 
 void aes128_ctx_release(aes128ctx *r);
 
@@ -40,7 +41,7 @@ void aes256_ctr_keyexp(aes256ctx *r, const unsigned char *key);
 
 void aes256_ecb(unsigned char *out, const unsigned char *in, size_t nblocks, const aes256ctx *ctx);
 
-void aes256_ctr(unsigned char *out, size_t outlen, const unsigned char *iv, uint32_t ctr, const aes256ctx *ctx);
+void aes256_ctr(unsigned char *out, size_t outlen, const unsigned char *iv, const aes256ctx *ctx);
 
 void aes256_ctx_release(aes256ctx *r);
 
